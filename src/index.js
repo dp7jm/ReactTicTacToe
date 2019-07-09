630901e83783791a1cc3f5c2fca948a13067807d
{dp7jm} <{dpjm@chef.net}>
2019-07-09 23:59:19 +0200

    added constructor with this.state.square array

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ class Square extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            value: props.value,
+            value: null,
         };
     }
     render() {
       return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
+        <button 
+        className="square" 
+        onClick={() => this.setState({value: 'X'})}
+        >
           {this.state.value}
         </button>
       );
@@ -19,6 +22,12 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+      constructor(props){
+          super(props);
+          this.state = {
+              squers: Array(9).fill(null),
+          }
+      }
     renderSquare(i) {
       return <Square value={i} />;
     }
